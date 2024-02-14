b7cce9841136b26dd4ed798e0e9e575593b4c82e
yashdeep <yashdeep@clevertap.com>
2024-02-14 18:16:27 +0530

    added version

--- src/main/java/tutorial/PlanSpec.java
+++ src/main/java/tutorial/PlanSpec.java
@@ -189,9 +189,9 @@ public class PlanSpec {
                                     "echo '{\"planResultKey\" : \"'${bamboo_yash_DashBuildResultKey}'\", \"name\" : \"'release-$bamboo_yash_DashBuildResultKey'\"}' > data.json\n" +
                                     "cat data.json\n" +
                                     "version=$(curl -X POST 'http://13.201.61.172:8085/rest/api/latest/deploy/project/1015809/version' --header \"Authorization: Bearer $bamboo_clienttoken\"  -H \"Accepts: application/json\" -H \"Content-Type: application/json\" --data-raw \"$(cat data.json)\" | jq -r '.id')\n" + 
-                                    "echo $version\n" +
+                                    "echo Version : $version\n" +
                                     "deployresulturl=$(curl -X POST \"http://13.201.61.172:8085/rest/api/latest/queue/deployment/?environmentId=1081345&versionId=$version\" --header \"Authorization: Bearer $bamboo_clienttoken\" -H \"Accepts: application/json\" | jq '.')\n" +
-                                    "echo $deployresulturl\n" 
+                                    "echo Deployment: $deployresulturl\n" 
                                 )                                
                  ),
                  new Job("Trigger NB Deployment","DEPLOYNBJOB").tasks(
