78b4192d383097383c1d0593c7a39dbb452e67ee
yashdeep <yashdeep@clevertap.com>
2024-02-14 12:55:56 +0530

    Updated Bamboo Server

--- src/main/java/tutorial/PlanSpec.java
+++ src/main/java/tutorial/PlanSpec.java
@@ -31,11 +31,9 @@ public class PlanSpec {
     public static void main(String[] args) throws Exception {
         // by default credentials are read from the '.credentials' file
         BambooServer bambooServer = new BambooServer("http://13.235.94.197:8085");
-
         Plan plan1 = new PlanSpec().createPlan1("Dashboard","DASH");
         Plan plan2 = new PlanSpec().createPlan2("Notification-Backend","NB");
         Plan plan3 = new PlanSpec().createPlan3("Automated-Tests","AUTOTEST");
-
         Deployment deployment1=new PlanSpec().createDeploymentProject1("PROJ","DASH");
         Deployment deployment2=new PlanSpec().createDeploymentProject2("PROJ","NB");
         
